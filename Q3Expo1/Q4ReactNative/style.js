b4e6f5b34e71578750699d5f2e2b6c87d01be88f
NgoLuc11 <luc.ngo.set18@eiu.edu.vn>
2023-10-25 10:49:27 +0700

    v1

---
+++ Q3Expo1/Q4ReactNative/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+export default style = StyleSheet.create({
+    container: { backgroundColor: "#fff"},
+    box: {
+        width: 100,
+        height: 100,
+        justifyContent: "center",
+        margin: 20,
+    },
+});
